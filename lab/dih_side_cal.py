ea8891893c3feaed1d26ea5f4574653741c5daec
MiyawakiS <miyawaki@ecstudio-academy.jp>
2016-05-07 16:36:41 +0900

    Adding dih_side_cal.py

--- lab/dih_side_cal.py
+++ lab/dih_side_cal.py
@@ -3,12 +3,28 @@
 
 #Syn計算
 def calculateSyn(fName):
+    for file in open(fName,"r"):
+        data = file.split()　#"r" -> 読み出し
+        value = float(data[1])
+
+        if -90.0 < value <90.0:
+            sum_syn += 1
+
+    ave_syn = sum_syn / 10000.0
+    return ave_syn
 
 
 #Anti計算
 def calculateAnti(fName):
+    for file in open(fName,"r"):
+        data = file.split()　#"r" -> 読み出し
+        value = float(data[1])
 
+        if value < -90.0 or value > 90.0:
+            sum_anti += 1
 
+    ave_anti = sum_anti / 10000.0
+    return ave_anti
 
 
 #main関数
