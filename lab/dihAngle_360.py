17d6ff52c56f7ae80d31b5e7f617de86619601ac
Onadius <orezigokusyounenn@gmail.com>
2016-08-02 12:23:26 +0900

    コマンドの自動実行関数作成

--- lab/dihAngle_360.py
+++ lab/dihAngle_360.py
@@ -1,16 +1,34 @@
 #coding: UTF-8
 import subprocess
+import datetime
 
+#make textFile for tleap*360 function
+def writeCommand(dirName, com1, com2, com3, com4, com5_1, com5_2, com6):
 
-#make textFile for tleap*360
-def writeCommand(dirName, com1, com2, com3, com4, com5, com6):
-
-    	f = open("doCommand.txt", "w")
-    	for angle in range(-180, 181):
+	fname = "doCommand.txt" #define a file_name
+    	f = open(fname, "w")
+    	
+    	for angle in range(-180, -179): #(-180, 181)
+    		#shaped command_str
 		comAngle = com4 + str(angle) + " }}"
+		comSave = com5_1 + com5_2 + str(angle) + ".inpcrd" 
+		
 		f.write(com1 + "\n" + com2 + "\n" + com3 + "\n" + comAngle 
-                       + "\n" + com5 + "\n" + com6 + "\n")
+                       + "\n" + comSave + "\n" + com6 + "\n")
+                       
+	return fname
+
+
+
+#Execute command function
+def doCommand(fname, year, month, day):
 	
+	#Confirmation of txtFile
+    	subprocess.check_call("ls")
+    	subprocess.check_call(["cat", fname])
+    	
+    	#subprocess.check_call(["", ""])
+
 
 
 #main function
@@ -23,11 +41,17 @@ if __name__ == "__main__":
 	com2 = "set default PBradii mbondi2"
 	com3 = "test = sequence{ACE ASH NME}"
 	com4 = "impose test {2} {{OE1 CG OE2 HE2 "
-	com5 = "saveamberparm test confamb.prmtop confamb.inpcrd"
+	com5_1 = "saveamberparm test" 
+	com5_2 = " confamb.prmtop confamb"
 	com6 = "quit"
+	
+	#get today_data
+	d = datetime.datetime.today()
+	year = str(d.year)
+	month = str(d.month)
+	day = str(d.day)
+	
+	fname = writeCommand(dirName, com1, com2, com3, com4, com5_1, com5_2, com6) #makefile
+	doCommand(fname, year, month, day) #Execute command
+	
 
-	writeCommand(dirName, com1, com2, com3, com4, com5, com6) #create File
-
- 	#Confirmation of txtFile
-    	subprocess.check_call("ls")
-    	subprocess.check_call(["cat", "doCommand.txt"])
