842fbd6b8c79c68973a24e63b75fb4b2cdf3d9c9
Onadius <orezigokusyounenn@gmail.com>
2016-08-05 12:59:05 +0900

    resulyInpcrdディレクトリの自動生成etc...

--- lab/dihAngle_360.py
+++ lab/dihAngle_360.py
@@ -1,57 +1,66 @@
 #coding: UTF-8
 import subprocess
 import datetime
+import os
 
 #make textFile for tleap*360 function
-def writeCommand(dirName, com1, com2, com3, com4, com5_1, com5_2, com6):
+def writeCommand(com2, com3, com4, com5_1, com5_2):
 
 	fname = "doCommand.txt" #define a file_name
     	f = open(fname, "w")
     	
-    	for angle in range(-180, -179): #(-180, 181)
+    	for angle in range(-180, 181): #(-180, 181)
     		#shaped command_str
 		comAngle = com4 + str(angle) + " }}"
 		comSave = com5_1 + com5_2 + str(angle) + ".inpcrd" 
 		
-		f.write(com1 + "\n" + com2 + "\n" + com3 + "\n" + comAngle 
-                       + "\n" + comSave + "\n" + com6 + "\n")
+		f.write(com2 + "\n" + com3 + "\n" + comAngle 
+                       + "\n" + comSave + "\n")
                        
 	return fname
 
 
 
 #Execute command function
-def doCommand(fname, year, month, day):
+def doCommand(fname, year, month, day, sec, com1):
 	
-	#Confirmation of txtFile
-    	subprocess.check_call("ls")
-    	subprocess.check_call(["cat", fname])
     	
-    	#subprocess.check_call(["", ""])
-
+    	#make new dir for resultInpcrd
+    	new_dir = "resultInpcrd_"+ month + day + sec
+    	cmd1 = "mkdir %s" % str(new_dir)
+    	cmd2 = "cd %s" % str(new_dir)
+    	
+    	subprocess.check_call(cmd1.strip().split(" "))
+    	
+    	#Confirmation
+    	subprocess.check_call("ls")	
+    	if(os.path.exists(str(new_dir)) == True):
+    		print("> %s is exists!" % str(new_dir))
+    		#subprocess.check_call(cmd2.strip().split(" "), shell=True)
+    		#subprocess.check_call(com1.strip().split(" "))
 
 
 #main function
 if __name__ == "__main__":
 	
-    	dirName = raw_input("What is the name of Dir? >") #
-
+    	dirName = raw_input("What is the name of Dir? >") 
+    	
 	#variable for command
-	com1 = "tleap -s -f leaprc." + dirName
+	com1 = "tleap -s -f leaprc.%s" % (dirName)
 	com2 = "set default PBradii mbondi2"
 	com3 = "test = sequence{ACE ASH NME}"
 	com4 = "impose test {2} {{OE1 CG OE2 HE2 "
 	com5_1 = "saveamberparm test" 
 	com5_2 = " confamb.prmtop confamb"
-	com6 = "quit"
 	
 	#get today_data
 	d = datetime.datetime.today()
 	year = str(d.year)
 	month = str(d.month)
 	day = str(d.day)
+	sec = str(d.second)
 	
-	fname = writeCommand(dirName, com1, com2, com3, com4, com5_1, com5_2, com6) #makefile
-	doCommand(fname, year, month, day) #Execute command
+	fname = writeCommand(com2, com3, com4, com5_1, com5_2) #makefile
+	doCommand(fname, year, month, day, sec, com1) #Execute command
 	
 
